32ba3e9a2cb0dc6128af874513b3402961f05cb9
Herman Cheng <34428141+smashy09@users.noreply.github.com>
2020-04-19 18:42:39 -0700

    card html css done

--- comps/Card/index.js
+++ comps/Card/index.js
@@ -1,7 +1,11 @@
 import React from 'react';
 import './card.css';
 
-const Card = ({header, question, backgroundColor, healthytxt, balancedtxt, unhealthytxt}) => <div
+const healthyImg = require('./healthyicon.png');
+const balancedImg = require('./balancedicon.png');
+const unhealthyImg = require('./unhealthyicon.png');
+
+const Card = ({header, question, backgroundColor, healthytxt, balancedtxt, unhealthytxt, healthyImg, balancedImg, unhealthyImg}) => <div
 style={{backgroundColor:backgroundColor}}
 className="survey_card"> 
     <div class="card_header">
@@ -12,9 +16,20 @@ className="survey_card">
     </div>
 
     <div class="card_answers">
-        <div class="options">{healthytxt} </div>
-        <div class="options">{balancedtxt} </div>
-        <div class="options">{unhealthytxt} </div>
+        <div class="options">
+            <img src={healthyImg} />
+            {healthytxt} 
+        </div>
+
+        <div class="options">
+           <img src={balancedImg} />
+            {balancedtxt}
+         </div>
+
+        <div class="options">
+           <img src={unhealthyImg} />
+            {unhealthytxt} 
+        </div>
     </div>
 
 </div>
@@ -24,7 +39,10 @@ Card.defaultProps = {
     question:"How much do you sit down a day?",
     healthytxt:"1-2 Hours",
     balancedtxt:"3-5 Hours",
-    unhealthytxt:"7+ Hours"
+    unhealthytxt:"7+ Hours",
+    healthyImg:healthyImg,
+    balancedImg:balancedImg,
+    unhealthyImg:unhealthyImg
 }
 
 export default Card;
