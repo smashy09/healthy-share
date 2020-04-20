28edd13134d380e1ceb10ebfa08282bb0e1a5c2b
Herman Cheng <34428141+smashy09@users.noreply.github.com>
2020-04-19 19:04:38 -0700

    card component html css complete

--- comps/Card/index.js
+++ comps/Card/index.js
@@ -5,7 +5,7 @@ const healthyImg = require('./healthyicon.png');
 const balancedImg = require('./balancedicon.png');
 const unhealthyImg = require('./unhealthyicon.png');
 
-const Card = ({header, question, backgroundColor, healthytxt, balancedtxt, unhealthytxt, healthyImg, balancedImg, unhealthyImg}) => <div
+const Card = ({header, question, backgroundColor, healthytxt, balancedtxt, unhealthytxt, healthyImg, balancedImg, unhealthyImg, onClick}) => <div
 style={{backgroundColor:backgroundColor}}
 className="survey_card"> 
     <div class="card_header">
@@ -16,17 +16,17 @@ className="survey_card">
     </div>
 
     <div class="card_answers">
-        <div class="options">
+        <div class="options" onClick={AnswerClick}>
             <img src={healthyImg} />
             {healthytxt} 
         </div>
 
-        <div class="options">
+        <div class="options" onClick={AnswerClick}>
            <img src={balancedImg} />
             {balancedtxt}
          </div>
 
-        <div class="options">
+        <div class="options" onClick={AnswerClick}>
            <img src={unhealthyImg} />
             {unhealthytxt} 
         </div>
@@ -34,7 +34,12 @@ className="survey_card">
 
 </div>
 
+function AnswerClick() {
+    alert("hi")
+}
+
 Card.defaultProps = {
+    backgroundColor:"white",
     header:"Question #1",
     question:"How much do you sit down a day?",
     healthytxt:"1-2 Hours",
@@ -42,7 +47,8 @@ Card.defaultProps = {
     unhealthytxt:"7+ Hours",
     healthyImg:healthyImg,
     balancedImg:balancedImg,
-    unhealthyImg:unhealthyImg
+    unhealthyImg:unhealthyImg,
+    onClick:AnswerClick
 }
 
 export default Card;
