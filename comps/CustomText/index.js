10b0d4769b2394c1c13584f970c9bc02e2e9a211
Herman Cheng <34428141+smashy09@users.noreply.github.com>
2020-04-21 18:14:34 -0700

    survey page1 done

--- comps/CustomText/index.js
+++ comps/CustomText/index.js
@@ -5,12 +5,12 @@ import './text.css';
 //text
 
 const CustomText = ({text, fontSize, color}) => <div className="custom_text_box">
-<p>{text}</p>
+<p style={{fontSize, color}}> {text} </p>
 </div>
 
 CustomText.defaultProps = {
     text:"Hello There!",
-    fontSize:10,
+    fontSize:15,
     color:"#3A3D42"
 }
 
--- pages/Survey1/index.js
+++ pages/Survey1/index.js
@@ -4,17 +4,29 @@ import './survey1.css';
 import Header from '../../comps/Header';
 import HamburgerMenu from '../../comps/HamburgerMenu';
 import CustomText from '../../comps/CustomText';
+import CustomButton from '../../comps/CustomButton';
 
 
-const Survey1 = ({}) => <div>
-<Card />
+const Survey1 = ({backgroundColor}) => <div 
+style={{backgroundColor}}
+>
 <Header />
-<CustomText fontSize={32} text="Survey"/>
 {/* <HamburgerMenu /> */}
+
+<div class="survey_container">
+<CustomText fontSize={32} text="Survey"/>
+<Card />
 </div>
 
-Survey1.defaultProps = {
+<div class="survey_next">
+<CustomButton/>
+</div>
 
+</div>
+
+
+Survey1.defaultProps = {
+    backgroundColor:"#F3F2EE"
 }
 
 export default Survey1;
