88a637953cd7eac3648ce781993d79aa17f3423c
semileeBCIT <55604874+semileeBCIT@users.noreply.github.com>
2020-04-30 03:27:44 +0900

    Enter

---
+++ .history/comps/Card/index_20200430032606.js
@@ -0,0 +1,78 @@
+import React from 'react';
+import './card.css';
+
+const healthyImg = require('./healthyicon.png');
+const balancedImg = require('./balancedicon.png');
+const unhealthyImg = require('./unhealthyicon.png');
+
+const Card = ({header, question, backgroundColor, healthytxt, balancedtxt, unhealthytxt, healthyImg, balancedImg, unhealthyImg, onClick}) => <div
+style={{backgroundColor:backgroundColor}}
+className="survey_card"> 
+    <div className="card_header">
+        {header}
+    </div>
+    <div className="card_question">
+        {question}
+    </div>
+
+    <div className="card_answers">
+        <div className="options" onClick={AnswerClick}>
+        <div className="options1" onClick={Option1 }>
+            <img src={healthyImg} />
+            {healthytxt} 
+        </div>
+
+        <div className="options" onClick={AnswerClick}>
+           <img src={balancedImg} />
+            {balancedtxt}
+         </div>
+
+        <div className="options" onClick={AnswerClick}>
+           <img src={unhealthyImg} />
+        <div className="options2" >
+           <img src={balancedImg} onClick={Option2} />
+            {balancedtxt}
+         </div>
+
+        <div className="options3" >
+           <img src={unhealthyImg} onClick={Option3} />
+            {unhealthytxt} 
+        </div>
+    </div>
+
+</div>
+
+function Option1() {
+    document.querySelector(".options1").style.backgroundColor = "grey"
+    document.querySelector(".options2").style.backgroundColor = "initial"
+    document.querySelector(".options3").style.backgroundColor = "initial"
+}
+
+function Option2() {
+    document.querySelector(".options1").style.backgroundColor = "initial"
+    document.querySelector(".options2").style.backgroundColor = "grey"
+    document.querySelector(".options3").style.backgroundColor = "initial"
+}
+
+function Option3() {
+    document.querySelector(".options1").style.backgroundColor = "initial"
+    document.querySelector(".options2").style.backgroundColor = "initial"
+    document.querySelector(".options3").style.backgroundColor = "grey"
+}
+
+Card.defaultProps = {
+    backgroundColor:"white",
+    header:"Question #1",
+    question:"How much do you sit down a day?",
+    healthytxt:"1-2 Hours",
+    balancedtxt:"3-5 Hours",
+    unhealthytxt:"7+ Hours",
+    healthyImg:healthyImg,
+    balancedImg:balancedImg,
+    unhealthyImg:unhealthyImg,
+    onClick:Option1,
+    onClick:Option2,
+    onClick:Option3
+}
+
+export default Card;
---
+++ .history/pages/Survey1/index_20200430032606.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import Card from '../../comps/Card';
+import './survey1.css';
+import Header from '../../comps/Header';
+import CustomText from '../../comps/CustomText';
+import CustomButton from '../../comps/CustomButton';
+import Router from 'next/router';
+
+function SurveyNext1(){
+    Router.push("/Survey2");
+}
+
+const Survey1 = () => <div>
+<Header />
+{/* <HamburgerMenu /> */}
+
+<div className="survey_container">
+<CustomText fontSize={32} text="Survey"/>
+<Card />
+</div>
+
+<div className="survey_next">
+ <CustomButton text="Next!" onClick={SurveyNext1} />
+</div>
+
+</div>
+
+
+export default Survey1;
