79d267204a9d9c0cfc421fbaac348fe6143c032f
JoshPaulson1 <55604836+JoshPaulson1@users.noreply.github.com>
2020-05-07 12:39:00 -0700

    fixes

---
+++ .history/pages/AnimationPage2/index_20200506214516.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import Animation from '../../comps/Animation';
+import './AnimationPage2.css';
+import Header from '../../comps/Header';
+import CustomText from '../../comps/CustomText';
+import CustomButton from '../../comps/CustomButton';
+import Router from 'next/router';
+
+function AnimationNext2(){
+    Router.push("/AnimationPage3");
+}
+
+function Animationback(){
+    Router.push("/AnimationPage1");
+}
+
+const AnimationPage2 = () => <div>
+<Header />
+{/* <HamburgerMenu /> */}
+
+<div className="Animation_container">
+<CustomText fontSize={18} text="There will be three multiply choice questions
+regarding your health state that needs to be
+answered for you to then receive health results.
+"/>
+</div>
+
+<div className="Animation_next">
+<CustomButton text="Back" onClick={Animationback} />
+ <CustomButton text="Next!" onClick={AnimationNext2} />
+</div>
+<div className="Animation_people">
+<Animation />
+</div>
+</div>
+
+
+export default AnimationPage2;
