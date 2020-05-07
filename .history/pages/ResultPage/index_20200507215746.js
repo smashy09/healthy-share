95875259fab49e635e6feafd0afb1add2097d74c
semileeBCIT <55604874+semileeBCIT@users.noreply.github.com>
2020-05-07 22:00:38 +0900

    midway

---
+++ .history/pages/ResultPage/index_20200507215746.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import './resultpage.css';
+import Header from '../../comps/Header';
+import CustomButton from '../../comps/CustomButton';
+import ResultCard from '../../comps/ResultCard';
+import Results from '../../comps/Results';
+import Router from 'next/router';
+
+function GoHome(){
+    Router.push("/HomePage");
+}
+
+const ResultPage = ({backgroundColor, margin}) => <div style={{backgroundColor}}>
+
+    <div className="resultpage" >
+        <Header />
+        <div style={{margin:margin}}>
+        <ResultCard />
+        </div>
+        <Results />
+        <div className="gohome_button">
+            <CustomButton text="Start again!"  onClick={GoHome}/>
+        </div>
+    </div>
+</div>
+
+ResultPage.defaultProps = {
+    backgroundColor:"#F3F2EE",
+    margin:"20px 0"
+}
+
+export default ResultPage;
