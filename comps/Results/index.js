aeeef708ee5db415105c7394298c27ae3f3eeff1
Herman Cheng <34428141+smashy09@users.noreply.github.com>
2020-05-08 19:13:56 -0700

    array added for advice

--- comps/Results/index.js
+++ comps/Results/index.js
@@ -7,7 +7,7 @@ const workoutImg = require('./workout.svg');
 const lesscaloriesImg = require('./veggies.svg');
 const sleepImg = require('./sleep.svg');
 
-const Results = ({type, couchpotatoImg, result}) => <div className="results">
+const Results = ({type, couchpotatoImg, result, advice1, advice2, advice3}) => <div className="results">
     Your type: <span style={{fontSize:"20px",fontWeight:"bold"}}>{type}</span>
     <div className="resultImg">
         <img src={couchpotatoImg} />
@@ -20,19 +20,19 @@ const Results = ({type, couchpotatoImg, result}) => <div className="results">
         <div className="solution_box">
             <img src={workoutImg} /> 
             <div className="solution_text">
-            Try three days of strength training, two days of cardio, and two days of active rest.
+            {advice1}
             </div>
         </div>
         <div className="solution_box">
             <img src={lesscaloriesImg} />
             <div className="solution_text">
-            A minimum of 400g of fruit and veg should be eaten every day
+            {advice2}
             </div>
         </div>
         <div className="solution_box">
             <img src={sleepImg} />
             <div className="solution_text">
-            Get at least 7 hours of sleep. Avoid consuming alcohol or caffeine before bedtime. 
+            {advice3}
             </div>
         </div>
     </div>
@@ -45,6 +45,9 @@ Results.defaultProps = {
     workoutImg:workoutImg,
     lesscaloriesImg:lesscaloriesImg,
     sleepImg:sleepImg,
+    advice1:"Try three days of strength training, two days of cardio, and two days of active rest.",
+    advice2:"A minimum of 400g of fruit and veg should be eaten every day",
+    advice3:"Get at least 7 hours of sleep. Avoid consuming alcohol or caffeine before bedtime.",
     result:"Seems like you sit, eat too much and lack of sleep."
 }
 
