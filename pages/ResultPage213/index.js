1437660d103802362ae4a103c1b15d31f6327e5f
Herman Cheng <34428141+smashy09@users.noreply.github.com>
2020-05-08 20:13:43 -0700

    9 results added for middle

--- pages/ResultPage213/index.js
+++ pages/ResultPage213/index.js
@@ -21,9 +21,9 @@ const ResultPage = ({backgroundColor, margin}) => {
     return <div style={{backgroundColor}} className="resultpage" >
         <Header />
         <div style={{margin:margin, textAlign:"center"}}>
-        <ResultCard answer1="3-5 Hours" answer2="None" answer3="4- Hours"  />
+        <ResultCard answer1="3-5 Hours" answer2="None" answer3="7+"  />
         </div>
-        <Results advice1="You sit a healthy amount, but to improve try scraping another hour of sitting away and do some cardio exercises" advice2="A minimum of 400g of fruit and veg should be eaten every day" advice3="Get at least 7 hours of sleep. Avoid consuming alcohol or caffeine before bedtime." result="You sit a healthy amount, however, you are not eating healthy enough and lack sleep. " />
+        <Results advice1="You sit a decent amount, but to improve try scraping another hour of sitting away and do some cardio exercises" advice2="A minimum of 400g of fruit and veg should be eaten every day" advice3="You are getting a good amount of sleep! Keep within 7-10 hours to stay healthy." result="You sit a decent amount and get a healthy amount of sleep, however, you are not eating healthy enough " />
         <div className="gohome_button">
             <CustomButton text="Start again!"  onClick={GoHome}/>
         </div>
