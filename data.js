aae9c34a3878913ab6fc0736e6f41b0db94ac816
Herman Cheng <34428141+smashy09@users.noreply.github.com>
2020-05-14 20:05:21 -0700

    a

--- data.js
+++ data.js
@@ -26,15 +26,19 @@ export function ChangeData(d){
 export function Sumup(){
     if(data.resultsit === "7+ Hours" && data.resulteat === "None" && data.resultsleep === "4- Hours"){
         data.type = "Couch Potato";
-        data.result = "Seems like you sit, eat too much and lack sleep."
+        data.result = "Seems like you sit, dont eat enough vegetables and lack sleep."
     }
     else if(data.resultsit === "3-5 Hours" && data.resulteat === "None" && data.resultsleep === "4- Hours"){
         data.type = "Couch Potato";
-        data.result = "Seems like you sit an average amount, eat too much and lack sleep."
+        data.result = "Seems like you sit an average amount, dont eat enough vegetables and lack sleep."
     }
     else if(data.resultsit === "7+ Hours" && data.resulteat === "1 Cup" && data.resultsleep === "4- Hours"){
         data.type = "Couch Potato";
-        data.result = "Seems like you sit too much, eat too much and lack sleep."
+        data.result = "Seems like you sit too much, eat enough vegetables and lack sleep."
+    }
+    else if(data.resultsit === "7+ Hours" && data.resulteat === "1 Cup" && data.resultsleep === "4- Hours"){
+        data.type = "Couch Potato";
+        data.result = "Seems like you sit too much, eat enough vegetables and lack sleep."
     }
 }
 
