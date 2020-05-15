0903ce29414cd59c2c5443698eeb61c35181d1f9
JoshPaulson1 <55604836+JoshPaulson1@users.noreply.github.com>
2020-05-14 20:11:29 -0700

    hi

--- .history/pages/ResultPage312/index_20200514122327.js
+++ .history/pages/ResultPage312/index_20200514122327.js
@@ -5,10 +5,33 @@ import CustomButton from '../../comps/CustomButton';
 import ResultCard from '../../comps/ResultCard';
 import Results from '../../comps/Results';
 import Router from 'next/router';
+import {data, ChangeData} from '../../data';
 
 function GoHome(){
     Router.push("/HomePage");
 }
+
+// var index = 0;
+// const results_arr = [
+//     {
+//         healthytxt:"1-2 Hours",
+//         balancedtxt:"3-5 Hours",
+//         unhealthytxt:"7+ Hours",
+//         text:"abcde",
+//         img:""
+//     },
+//     {
+//         healthytxt:"2000 Cal",
+//         balancedtxt:"3000 Cal",
+//         unhealthytxt:"4000+ Cal"
+//     },
+//     {
+//         healthytxt:"7+ Hours",
+//         balancedtxt:"5-6 Hours",
+//         unhealthytxt:"3-4 Hours"
+//     }
+// ]
+
 
 const ResultPage = ({backgroundColor, margin}) => {
 
@@ -21,11 +44,14 @@ const ResultPage = ({backgroundColor, margin}) => {
     return <div style={{backgroundColor}} className="resultpage" >
         <Header />
         <div style={{margin:margin, textAlign:"center"}}>
-        <ResultCard answer1="1-2 Hours" answer2="None" answer3="5-6 Hours" />
+        <ResultCard answer1="1-2 Hours" answer2="1 Cup" answer3="4-"  />
         </div>
         <Results advice1="You are sitting a healthy amount! Great work, try improving your blood flow by doing cardio."
 
- advice2="A minimum of 400g of fruit and veg should be eaten every day" advice3="Get at least 7 hours of sleep. Avoid consuming alcohol or caffeine before bedtime." result="You sit a healthy amount, have an average diet and sleep a decent amount. " />
+ advice2="Try implementing another cup of vegetables throughout your daily meals. Carrots make for a great snack!" advice3="Get at least 7 hours of sleep. Avoid consuming alcohol or caffeine before bedtime." result="You sit a healthy amount, have an average diet and sleep a decent amount. " />
+        <ResultCard />
+        </div>
+        <Results />
         <div className="gohome_button">
             <CustomButton text="Start again!"  onClick={GoHome}/>
         </div>
