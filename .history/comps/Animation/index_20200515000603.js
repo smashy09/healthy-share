def861bf20735bedb3690e5e23aa879fec488ca6
JoshPaulson1 <55604836+JoshPaulson1@users.noreply.github.com>
2020-05-15 00:37:13 -0700

    color changes

---
+++ .history/comps/Animation/index_20200515000603.js
@@ -0,0 +1,38 @@
+import React from 'react'; 
+import './animation.css'; 
+ 
+const animationguy= require('./guywaving.png'); 
+const animationgirl= require('./girlwaving.png'); 
+const animationarmguy= require('./guyArm.png'); 
+const animationarmgirl= require('./armgirl.png'); 
+
+
+
+const Animation = ({animationguy,animationgirl,animationarmguy,animationarmgirl}) => <div 
+className="Animation_Page">  
+<div class="girl"> 
+           <img src={animationgirl} /> 
+         </div> 
+         <div className="armgirl"> 
+           <img src={animationarmgirl} /> 
+           </div>
+  <div class="guy"> 
+           <img src={animationguy} /> 
+           </div> 
+           <div class="armguy"> 
+           <img src={animationarmguy} /> 
+           </div>
+</div> 
+
+ 
+Animation.defaultProps = { 
+    animationguy:animationguy, 
+    animationgirl:animationgirl,
+    animationarmguy:animationarmguy,
+    animationarmgirl:animationarmgirl
+} 
+
+ 
+ 
+
+export default Animation; 
---
+++ .history/pages/HomePage/index_20200514204629.js
@@ -0,0 +1,29 @@
+import Router from 'next/router';
+import React from 'react';
+import './home.css';
+import CustomText from '../../comps/CustomText';
+import CustomButton from '../../comps/CustomButton';
+
+
+
+function NextPage(){
+    Router.push("/InformationPage");
+}
+//homePage
+const img = require('./logo.png');
+const HomePage = () => <div>
+<div id="All_home_page">
+    <CustomText fontSize={72} color="#A2C5AC" text="Healthy-U"/>
+<img src={img} className="image"/>
+<CustomText fontSize={32}  text="Become a better U today!"/>
+<div className="start_button">
+<CustomButton text="Start App!"  onClick={NextPage}/>
+</div>
+<CustomText fontSize={22} color="#A2C5AC" text="This app will give you the information you need to become a healthier U!"/>
+</div>
+</div>
+
+HomePage.defaultProps = {
+    img:img
+}
+export default HomePage;
